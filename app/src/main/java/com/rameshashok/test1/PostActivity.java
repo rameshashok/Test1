44afeb08bf891a0843add932f9024161716ceee7
rameshashok <contactrameshashok@gmail.com>
2017-11-10 11:43:32 +0530

    posting page is complete and it stores the content in the database.

--- app/src/main/java/com/rameshashok/test1/PostActivity.java
+++ app/src/main/java/com/rameshashok/test1/PostActivity.java
@@ -1,13 +1,110 @@
 package com.rameshashok.test1;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PostActivity extends AppCompatActivity {
 
+    // declaring variables
+    private ImageButton mSelectImage;
+    private EditText mPostTitle;
+    private EditText mPostDesc;
+    private Button mSubmitBtn;
+    private Uri mImageUri = null;
+    private StorageReference mStorage;
+    private DatabaseReference mDatabase;
+    private ProgressDialog mProgress;
+
+    private static final int GALLERY_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // system stuff
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        // gets reference for the firebase root directories
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
+
+        // initializing the progress dialog
+        mProgress = new ProgressDialog(this);
+
+        // assigning members from ui to variables
+        mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
+        mPostTitle = (EditText) findViewById(R.id.titleField);
+        mPostDesc = (EditText) findViewById(R.id.descField);
+        mSubmitBtn = (Button) findViewById(R.id.submitBtn);
+
+        // creating onclick event listener for the image
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);                       // getting a image from the gallery
+                galleryIntent.setType("image/*");                                                   // setting the intent as images to show all images in the phone
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);                             // starting the image activity
+            }
+        });
+
+        // creating onclick event listener for the submit button
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPosting();
+            }
+        });
+    }
+
+    private void startPosting() {
+        // showing progress for posting the content into database
+        mProgress.setMessage("Posting........");
+        mProgress.show();
+
+        final String title_val = mPostTitle.getText().toString().trim();
+        final String desc_val = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null) {   // if both the title and description is not empty
+            StorageReference filepath = mStorage.child("Blog_Images").child(mImageUri.getLastPathSegment());
+
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    DatabaseReference newPost = mDatabase.push();
+                    newPost.child("title").setValue(title_val);
+                    newPost.child("desc").setValue(desc_val);
+                    newPost.child("image").setValue(downloadUrl.toString());
+                    mProgress.dismiss();
+                    startActivity(new Intent(PostActivity.this, MainActivity.class));
+                }
+            });
+        }
+    }
+
+    // when the image intent gets an image from the gallery, this method will run
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {                            // if the image intent returns an image
+            mImageUri = data.getData();                                                             // get the image's uri
+            mSelectImage.setImageURI(mImageUri);                                                    // setting the image into our app
+        }
     }
 }
